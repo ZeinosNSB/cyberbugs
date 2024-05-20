9a0636e57a5d79584b3e0974b7cdf6afa89178d3
zeinosnsb <chinhthantoc@gmail.com>
2024-05-20 19:09:24 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
+import './index.css'
+
+import { ConfigProvider } from 'antd'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 import { Provider } from 'react-redux'
+
+import App from './App.jsx'
 import store from './store/store'
-import { ConfigProvider } from 'antd'
 import { config } from './theme/config'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
